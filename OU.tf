b0a8cd0e5f637fed0057e5c174bf0a8dd419888a
Nolan Heimlich <nolan.heimlich@gmail.com>
2020-12-03 20:07:41 -0500

    org account

--- OU.tf
+++ OU.tf
@@ -2,6 +2,7 @@ resource "aws_organizations_account" "account" {
   name  = "nhlabs"
   email = "nolan@nhlabs.org"
 }
+/*
 resource "aws_organizations_organization" "org" {
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
@@ -14,3 +15,4 @@ resource "aws_organizations_organizational_unit" "nhlabs" {
   name      = "students"
   parent_id = aws_organizations_organization.org.roots[0].id
 }
+*/
