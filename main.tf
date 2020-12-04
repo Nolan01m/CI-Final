a71dd6440856917261a46cd43db1942c6eab2820
Nolan Heimlich <nolan.heimlich@gmail.com>
2020-12-03 20:05:08 -0500

    adding organization

--- main.tf
+++ main.tf
@@ -32,15 +32,3 @@ resource "aws_instance" "Splunk_Instance" {
     Name = "Splunk_Instance"
   }
 }
-resource "aws_organizations_organization" "org" {
-  aws_service_access_principals = [
-    "cloudtrail.amazonaws.com",
-    "config.amazonaws.com",
-  ]
-
-  feature_set = "ALL"
-}
-resource "aws_organizations_organizational_unit" "nhlabs" {
-  name      = "nhlabs.org"
-  parent_id = aws_organizations_organization.org.roots[0].id
-}
