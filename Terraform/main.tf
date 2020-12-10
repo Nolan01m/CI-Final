434253827860af32fae3f828210642407bd8ff65
Nolan Heimlich <nolan.heimlich@gmail.com>
2020-12-10 01:01:16 -0500

    ..

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -32,3 +32,48 @@ resource "aws_instance" "Splunk_Instance" {
     Name = "Splunk_Instance"
   }
 }
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com",
+  ]
+
+  feature_set = "ALL"
+}
+
+
+
+/*
+resource "aws_organizations_account" "account" {
+  name  = "nhlabs"
+  email = "nolan@nhlabs.org"
+}
+
+resource "aws_organizations_organizational_unit" "nhlabs" {
+  name      = "students"
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
+*/
+
+/*
+resource "aws_organizations_account" "account" {
+  name  = "nhlabs"
+  email = "nolan@nhlabs.org"
+}
+
+resource "aws_organizations_organizational_unit" "nhlabs" {
+  name      = "students"
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
+*/
+/*
+# Ansible requires Python to be installed on the remote machine as well as the local machine.
+  provisioner "remote-exec" {
+    inline = ["sudo apt-get -qq install python -y"]
+  }
+  
+  connection {
+    private_key = "${file(var.private_key)}"
+    user        = "ubuntu"
+  }
+  */
